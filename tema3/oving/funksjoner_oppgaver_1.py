ccae4e79d987888c4595294b2389fc0f48597e02
Crane Operator <karad0152@uis.no>
2026-09-08 21:59:09 +0200

    oving funksjoner

--- tema3/oving/funksjoner_oppgaver_1.py
+++ tema3/oving/funksjoner_oppgaver_1.py
@@ -11,12 +11,12 @@ def velkomst(navn):
 
 def areal_rektangel(lengde, bredde):
     """Oppgave 2: Returner arealet av et rektangel."""
-    pass
+    return lengde * bredde
 
 
 def omkrets_rektangel(lengde, bredde):
     """Oppgave 3: Returner omkretsen av et rektangel."""
-    pass
+    return lengde * 2 + bredde * 2
 
 
 def celsius_til_fahrenheit(celsius):
@@ -24,11 +24,18 @@ def celsius_til_fahrenheit(celsius):
 
     Formel: fahrenheit = celsius * 9 / 5 + 32
     """
-    pass
+    fahrenheit = celsius * 9/5 + 32
+    print(f"{celsius} Celsius -> {fahrenheit} Fahrenheit")
 
+def fahrenheit_til_celsius(fahrenheit):
+    """Returnerer temperatur fra fahrenheit til celsius."""
 
-# Skriv egne funksjonskall her for a teste koden din.
-# Eksempel:
-# velkomst("Ada")
+    celsius = (fahrenheit - 32) * (5/9)
+    print(f"{fahrenheit} Fahrenheit -> {celsius} celsius ")
+
 
 velkomst("Kajus")
+print(f"Arealet for gitt rektangel er gitt ved: {areal_rektangel(2,5)}")
+print(f"Omkretsen til en rektangel med gitt verdier: {omkrets_rektangel(2,5)}")
+celsius_til_fahrenheit(20)
+fahrenheit_til_celsius(68)
