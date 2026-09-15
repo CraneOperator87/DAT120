21c688e53df0bc6387a7a194734d08b096b731e6
Crane Operator <karad0152@uis.no>
2026-09-15 21:37:04 +0200

    fortsettelse

--- tema4/obligatorisk/oppgave.py
+++ tema4/obligatorisk/oppgave.py
@@ -2,7 +2,7 @@ fil = open("tema4/obligatorisk/bronntrykk.txt", "r", encoding="utf-8")
 
 ignorerteLinjer = 0
 
-for linje in fil:
+for linje in fil: 
     linje = linje.strip() # 1.3 : Hver linje fjernes \n 
 
     if linje == "": # 1.4: Hoppe over tomme linjer
@@ -10,7 +10,20 @@ for linje in fil:
     if linje.startswith("#"): # 1.5: Hoppe over kommentarer
         ignorerteLinjer +=1; continue
 
-    print(linje)
+    deler = linje.split()
+    if len(deler) != 2: 
+        print(f"Feil Format: {linje}")
+        continue
+    talltekst = deler[0]
+    enhet = deler[1]
+
+    talltekst = talltekst.replace("," , ".")
+
+    try: 
+        verdi = float(talltekst) 
+        print(f"Gydlig verdi + enhet: {verdi} {enhet}")
+    except ValueError: 
+        print(f"Feil i linja!: {linje} ")
 
 fil.close()
 
